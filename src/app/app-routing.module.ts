bda10251865b79494cc3b9477d6eab3a2336e746
theolympicdream <theolympicdream2020@gmail.com>
2020-06-06 15:12:58 +0530

    added home component and initial configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+// Components
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
